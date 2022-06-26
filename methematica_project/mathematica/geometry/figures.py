9bd52d8ce29748afb4a758698f1f764baa24f292
rkorzen <korzeniewski@gmail.com>
2022-06-26 16:56:36 +0200

    end day 2

---
+++ methematica_project/mathematica/geometry/figures.py
@@ -0,0 +1,2 @@
+def square_area(side: int) -> float:
+    return side**2
---
+++ methematica_project/tests/test_geometry.py
@@ -0,0 +1,5 @@
+from mathematica.geometry.figures import square_area
+
+
+def test_square_area():
+    assert square_area(2) == 4
